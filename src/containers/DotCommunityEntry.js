d9e1d14720036f939f3ac280a0c3049ee1140302
sougatadafader <sougatadafader@gmail.com>
2019-10-10 00:05:57 -0400

    scaffolding

---
+++ src/containers/DotCommunityEntry.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch,Redirect} from "react-router-dom";
+
+
+class DotCommunityEntry extends React.Component {
+
+    state = {
+        loaded: true
+    };
+
+
+    render() {
+        if (!this.state.loaded) {
+            return null;
+        }
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" render={props => <Home/>}/>
+                        <Redirect to="/"/>
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
+
+}
+
+export default DotCommunityEntry;
