627101fbf5f0d21ceea24d0f4ef8b7b34019eae5
Sougata Dafader <sougatadafader@gmail.com>
2019-11-10 15:01:38 +0530

    Campaign UI CSS Updation

--- src/containers/CampaignSingle/CampaignSingle.js
+++ src/containers/CampaignSingle/CampaignSingle.js
@@ -32,7 +32,7 @@ class CampaignSingle extends React.Component
                         <div className="organiser-card card-ui">
                             <h3 className="volunteer-title">Organized By</h3>
                             <div className="media">
-                                <div className="user-dp-circle" style={{backgroundImage:`url(this.state.dpUrl)`}}></div>
+                                <div className="user-dp-circle" style={{backgroundImage:`url(${this.state.dpUrl})`}}></div>
                                 <div class="media-body">
                                     <h5>Sougata Dafader</h5>
                                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
