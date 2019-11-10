22a03f306a8ce976af10c4fe6cda9d77c5689802
Sougata Dafader <sougatadafader@gmail.com>
2019-11-10 14:23:06 +0530

    Campaign UI

--- src/containers/CampaignSingle/CampaignSingle.js
+++ src/containers/CampaignSingle/CampaignSingle.js
@@ -15,8 +15,30 @@ class CampaignSingle extends React.Component
         return(
             <div className="bigbro-container">
                 <Header />
-                <div className="container">
-                    <div className="col-12"><h3 className="text-center">Campaign Single</h3></div>
+                <div className="container space--top">
+                    <div className="col-8">
+                        <div className="campaign-card card-ui">
+                            <h3 className="campaign-title">Helping A Man In Need</h3>
+                            <div className="campaign-image">
+                                <img src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="img-responsive" />
+                            </div>
+                            <div className="campaign-meta">
+                                <p>Created 25 October 2019</p>
+                            </div>
+                            <div className="campaign-description">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere at urna quis maximus. Maecenas efficitur, turpis quis scelerisque consectetur, odio diam porta diam, id fringilla dolor justo eget diam. Aenean convallis interdum tincidunt. Proin ut tortor ut felis ornare venenatis at vel urna. Vivamus maximus, nunc vitae tristique vestibulum, mauris nulla egestas metus, at pretium diam augue non turpis. Donec id ultricies enim, quis gravida purus. Fusce placerat pellentesque imperdiet. Aenean vel magna ac augue blandit faucibus. Ut consequat imperdiet leo sed elementum. Pellentesque sed ligula id sem condimentum egestas at eu enim. Ut volutpat felis sed purus euismod, et euismod ante accumsan. Vestibulum vitae commodo quam, vel maximus odio.</p>
+                            </div>
+                        </div>
+                        <div className="organiser-card card-ui">
+
+                        </div>
+                        <div className="comments-card card-ui">
+
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        
+                    </div>
                 </div>
             </div>
         );
