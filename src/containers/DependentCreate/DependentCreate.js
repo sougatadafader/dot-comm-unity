33a62a89549e8c6f9695995052df258813859660
Sougata Dafader <sougatadafader@gmail.com>
2019-11-13 14:26:40 +0530

    Dependent Create

--- src/containers/DependentCreate/DependentCreate.js
+++ src/containers/DependentCreate/DependentCreate.js
@@ -14,29 +14,29 @@ class DependentCreate extends React.Component
                 <Header />
                 <div className="container space--top">
                     <div className="row">
-                        <div className="col-lg-8">
+                        <div className="col-lg-12">
                             <div className="campaign-create-card card-ui">
                                 <h3 className="campaign-create-title">Create A Person In Need</h3>
                                 <form>
-                                    <div className="form-group">
+                                    <div className="form-group row">
                                         <label className="col-lg-2 col-form-label">First Name</label>
                                         <div className="col-lg-10">
                                             <input type="text" name="firstname" className="form-control" placeholder="First Name" />
                                         </div>
                                     </div>
-                                    <div className="form-group">
+                                    <div className="form-group row">
                                         <label className="col-lg-2 col-form-label">Last Name</label>
                                         <div className="col-lg-10">
                                             <input type="text" name="lastname" className="form-control" placeholder="Last Name" />
                                         </div>
                                     </div>
-                                    <div className="form-group">
+                                    <div className="form-group row">
                                         <label className="col-lg-2 col-form-label">Landmark</label>
                                         <div className="col-lg-10">
                                             <input type="text" name="landmark" className="form-control" placeholder="Landmark" />
                                         </div>                                        
                                     </div>
-                                    <div className="form-group">
+                                    <div className="form-group row">
                                         <label className="col-lg-2 col-form-label">Zipcode</label>
                                         <div className="col-lg-10">
                                             <input type="text" name="zipcode" className="form-control" placeholder="Zipcode" />
@@ -46,7 +46,6 @@ class DependentCreate extends React.Component
                                 </form>
                             </div>
                         </div>
-                        <div className="col-lg-4"></div>
                     </div>
                 </div>
             </div>
