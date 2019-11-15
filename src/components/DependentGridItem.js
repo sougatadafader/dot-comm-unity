c3a3f5458d01f185692093341320838691ab9f05
Sougata Dafader <sougatadafader@gmail.com>
2019-11-15 15:15:09 +0530

    Dependent Create

--- src/components/DependentGridItem.js
+++ src/components/DependentGridItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DependentGridItem = ({dependent}) => {
     return(
         <div className="col-lg-4 dependent-grid-item">
-            <div className="dependent-grid-image"></div>
+            <div className="dependent-grid-image" style={{backgroundImage:`url(${dependent.imageUrl})`}}></div>
             <div className="dependent-grid-name">{dependent.firstName} {dependent.lastName}</div>
         </div>
     );
