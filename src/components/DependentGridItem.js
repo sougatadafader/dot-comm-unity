7cca9d7ab7d3c4bc2552333f47403fa0035f9408
Sougata Dafader <sougatadafader@gmail.com>
2019-11-17 15:51:39 +0530

    Dependent List View

--- src/components/DependentGridItem.js
+++ src/components/DependentGridItem.js
@@ -5,9 +5,11 @@ const DependentGridItem = ({dependent}) => {
         <div className="col-lg-12 dependent-grid-item">
             <div className="dependent-grid-image" style={{backgroundImage:`url(${dependent.imageUrl})`}}></div>
             <div className="dependent-grid-details">
-                <p>Name: <strong>{dependent.firstName} {dependent.lastName}</strong></p>
-                <p>Landmark: <strong>{dependent.landmark}</strong></p>
-                <p>Zipcode: <strong>{dependent.zipcode}</strong></p>
+                <div className="dependent-grid-details-inner">
+                    <p>Name: <strong>{dependent.firstName} {dependent.lastName}</strong></p>
+                    <p>Landmark: <strong>{dependent.landmark}</strong></p>
+                    <p>Zipcode: <strong>{dependent.zipcode}</strong></p>
+                </div>
             </div>
             <div className="clear"></div>
         </div>
