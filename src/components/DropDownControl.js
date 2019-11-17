e3e7a49e4d54520265b69cb442e9700d5c49c50a
Sougata Dafader <sougatadafader@gmail.com>
2019-11-18 03:24:56 +0530

    Create Campaign

--- src/components/DropDownControl.js
+++ src/components/DropDownControl.js
@@ -4,28 +4,43 @@ const DropDownControl = ({name,title,val,values,info,refreshList,loadingList}) =
     if(loadingList)
     {
         return(
-            <select className="form-control" name={name} required>
-                <option value="">Loading {title}...</option>
-            </select>
+            <div className="form-group row">
+                <label className="col-lg-2 col-form-label">{title}</label>
+                <div className="col-lg-10">
+                    <select className="form-control" name={name} required>
+                        <option value="">Loading {title}...</option>
+                    </select>
+                </div>
+            </div>
         );
     }
     else if(values.length > 0)
     {
         return(
-            <select className="form-control" defaultValue={val} name={name}>
-                <option value="">Select a {title}</option>
-            {
-                values.map((value,index)=>
-                    (<option key={index} value={value.value}>{value.displayText}</option>)
-                )
-            }
-        </select>
+            <div className="form-group row">
+                <label className="col-lg-2 col-form-label">{title}</label>
+                <div className="col-lg-10">
+                    <select className="form-control" defaultValue={val} name={name}>
+                        <option value="">Select a {title}</option>
+                        {
+                            values.map((value,index)=>
+                            (<option key={index} value={value.value}>{value.displayText}</option>)
+                            )
+                        }
+                    </select>
+                </div>
+            </div>
         );
     }
     return(
-        <select className="form-control" defaultValue={val} name={name}>
-            <option value="">No {title} found</option>
-        </select>
+        <div className="form-group row">
+            <label className="col-lg-2 col-form-label">{title}</label>
+            <div className="col-lg-10">
+                <select className="form-control" defaultValue={val} name={name}>
+                    <option value="">No {title} found</option>
+                </select>
+            </div>
+        </div>
     );
 }
 
