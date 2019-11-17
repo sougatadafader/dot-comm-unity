40e42eb83586722cc58c939dbd720c6842eba8c8
Sougata Dafader <sougatadafader@gmail.com>
2019-11-17 15:23:29 +0530

    Dependent List View

--- src/components/DependentGridItem.js
+++ src/components/DependentGridItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const DependentGridItem = ({dependent}) => {
     return(
-        <div className="col-lg-6 dependent-grid-item">
+        <div className="col-lg-12 dependent-grid-item">
             <div className="dependent-grid-image" style={{backgroundImage:`url(${dependent.imageUrl})`}}></div>
             <div className="dependent-grid-details">
-                
+
             </div>
             <div className="clear"></div>
         </div>
