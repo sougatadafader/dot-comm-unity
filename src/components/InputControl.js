ad8d592e3aa20d1aa14b06cf0a0f44ad4d20bc5f
Sougata Dafader <sougatadafader@gmail.com>
2019-11-18 00:41:26 +0530

    Create Input Control

--- src/components/InputControl.js
+++ src/components/InputControl.js
@@ -5,7 +5,7 @@ const InputControl = ({type,label,name,placeholder,val,inputChanged}) => {
         <div className="form-group row">
             <label className="col-lg-2 col-form-label">{label}</label>
             <div className="col-lg-10">
-                <input type={type} name={name} className="form-control" placeholder={placeholder} defaultValue={val} onChange={() => inputChanged()} required />
+                <input type={type} name={name} className="form-control" placeholder={placeholder} defaultValue={val} onChange={() => inputChanged(event)} required />
             </div>
         </div>
     );
