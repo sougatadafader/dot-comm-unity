c75773e01947a70965c644be53afc2c01aee6219
Sougata Dafader <sougatadafader@gmail.com>
2019-11-19 03:55:16 +0530

    Campaign Create

--- src/components/CampaignGridSingle.js
+++ src/components/CampaignGridSingle.js
@@ -1,15 +1,19 @@
+import React from 'react';
 import DonationProgress from '../components/DonationProgress';
 
 const CampaignGridSingle = ({campaign}) => {
     return(
         <div className="col-lg-4 campaign-grid-single">
-            <div className="campaign-grid-image" style={{backgroundImage:`url(${campaign.imageUrl})`}}></div>
-            <div className="campaign-grid-header">
-                <h2 className="campaign-grid-title"><a href="#">{campaign.header}</a></h2>
+            <div className="campaign-grid-inner">
+                <div className="campaign-grid-image" style={{backgroundImage:`url(${campaign.imageUrl})`}}></div>
+                <div className="campaign-grid-header">
+                    <h2 className="campaign-grid-title"><a href="#">{campaign.header}</a></h2>
+                </div>
                 <p className="campaign-grid-desc">{campaign.text}</p>
                 <DonationProgress progress="50%" />
                 <h3 className="donation-progress">$10000 <span>raised of $20000 target</span></h3>
             </div>
+            
         </div>
     );
 }
