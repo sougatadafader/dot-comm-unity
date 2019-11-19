2c7f1cc1ac40f865900b79c3f5df6649c4a10f17
Sougata Dafader <sougatadafader@gmail.com>
2019-11-20 01:21:56 +0530

    Campaign Single

--- src/components/CampaignCard.js
+++ src/components/CampaignCard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const CampaignCard = ({campaign}) => {
+    let created = campaign.created;
+    let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    let createdArr = created.split('T');
+    let createdDate = createdArr[0];
+    let createdDateArr = createdDate.split('-');
+    let dateShow = createdArr[2]+' '+months[createdDateArr[1]-1]+' '+createdDateArr[0];
     return(
         <div className="campaign-card card-ui">
             <h3 className="campaign-title">{campaign.header}</h3>
@@ -8,7 +14,7 @@ const CampaignCard = ({campaign}) => {
                     <img src={campaign.imageUrl} className="img-responsive" />
                 </div>
                 <div className="campaign-meta">
-                    <p>Created 25 October 2019</p>
+                    <p>Created {dateShow}</p>
                 </div>
                 <div className="campaign-description">
                     <p>{campaign.text}</p>
