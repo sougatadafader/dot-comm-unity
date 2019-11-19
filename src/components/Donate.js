274ab4ddb4c8d0114f4a562c6d36ff1d884ff8c5
Sougata Dafader <sougatadafader@gmail.com>
2019-11-20 03:20:21 +0530

    Campaign Single

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DonateButton from '../components/DonateButton';
+import DonateTextBox from './DonateTextBox';
 const Donate = ({user}) => {
     if(Object.keys(user).length == 0)
     {
@@ -10,15 +11,20 @@ const Donate = ({user}) => {
         );
     }
     return(
-        <form>
-            <div className="col-lg-4">
-                <DonateButton amount="30" />
-            </div>
-            <div className="col-lg-4">
-                <DonateButton amount="50" />
-            </div>
-            <div className="col-lg-4">
-                <DonateButton amount="100" />
+        <form className="donation-form">
+            <div className="row">
+                <div className="col-lg-4 donate-button-container">
+                    <DonateButton amount="30" />
+                </div>
+                <div className="col-lg-4 donate-button-container">
+                    <DonateButton amount="50" />
+                </div>
+                <div className="col-lg-4 donate-button-container">
+                    <DonateButton amount="100" />
+                </div>
+                <div className="col-lg-12">
+                    <DonateTextBox />
+                </div>
             </div>
         </form>
     );
---
+++ src/components/DonateTextBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DonateTextBox = () => {
+    return(
+        <div className="donate-input-box">
+            <div className="donate-input-box-inner">
+                <input type="number" className="donate-input" />
+            </div>
+        </div>
+    );
+}
+
+export default DonateTextBox;
