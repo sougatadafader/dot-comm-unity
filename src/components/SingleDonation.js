10bc5d263f995e18d0f685afde5157e3a8a50525
Sougata Dafader <sougatadafader@gmail.com>
2019-11-28 15:24:31 +0530

    Donation UI Modify

--- src/components/SingleDonation.js
+++ src/components/SingleDonation.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 const SingleDonation = ({donation}) => {
     return(
-        <div className="media">
-            <div className="user-dp-circle" style={{backgroundImage:`url(${donation.userImg})`}}></div>
-            <div className="media-body">
-                <h5>{donation.user}</h5>
-                <span className="donation-detail donation-amount">{donation.amount}$</span>
-                <span className="donation-detail donation-date">{donation.date}</span>
+        <div className="donation-panel panel-ui">
+            <div className="donation-panel-header panel-ui-header">
+                <div className="media">
+                    <div className="user-dp-circle half-size-dp" style={{backgroundImage:`url(${donation.userImg})`}}></div>
+                    <div className="media-body">
+                        <h5>{donation.user}</h5>
+                        <span className="donation-detail donation-amount">{donation.amount}$</span>
+                        <span className="donation-detail donation-date">{donation.date}</span>
+                    </div>
+                </div>
+            </div>
+            <div className="donation-panel-comment panel-ui-body">
+
             </div>
         </div>
+        
     );
 }
 
