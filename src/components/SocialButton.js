83d26a90bf0963172bd8763949d3a6c35c8ab9c4
Sougata Dafader <sougatadafader@gmail.com>
2019-11-28 21:50:41 +0530

    Like UI Modify

--- src/components/SocialButton.js
+++ src/components/SocialButton.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const SocialButton = ({platform,url}) => {
+const SocialButton = ({platform}) => {
     let linkClass = platform+'-button social-button';
     let iconClass = 'fa fa-'+platform;
+    let url = window.location.href;
+    if(platform == 'facebook')
+    {
+        url = 'https://facebook.com/sharer/sharer.php?u='+encodeURIComponent(url);
+    }
+    else if(platform == 'twitter')
+    {
+        url = 'https://twitter.com/intent/tweet?url='+encodeURIComponent(url);
+    }
     return(
         <span className="social-button-container">
             <a href={url} className={linkClass} target="_blank"><i className={iconClass}></i></a>
