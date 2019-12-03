2a227e835273071ab455d98ea0c8bba658dfdd53
sougatadafader <sougatadafader@gmail.com>
2019-12-03 09:01:00 -0500

    favicon change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import BigBrotherEntry from './containers/BigBrotherEntry'
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
