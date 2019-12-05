3fd524ae342b51ab777fd6dca4e0cf9b05d99666
Sougata Dafader <sougatadafader@gmail.com>
2019-12-05 16:31:15 +0530

    Donation History

--- src/components/DonationHistorySummary.js
+++ src/components/DonationHistorySummary.js
@@ -10,25 +10,35 @@ const DonationHistorySummary = ({nCampaigns,totalAmount,amountSteps}) => {
             setCampaignCount(campaigns => campaigns < nCampaigns?campaigns+1:campaigns);
             setIsFinished(isFinished => campaigns < nCampaigns || amount < totalAmount?false:true);
             setTimeout(updateCampaign,100);
-        },100);
+        },300);
         const aInterval = setTimeout(function updateAmount() {
             setAmount(amount => (amount+amountSteps) < totalAmount?(amount+amountSteps):totalAmount);
             setIsFinished(isFinished => (campaigns < nCampaigns || amount < totalAmount)?false:true);
             setTimeout(updateAmount,1);
-        },100);
+        },300);
         return () => {clearTimeout(cInterval); clearTimeout(aInterval);}
     },[isFinished]);
 
     return(
         <div className="container-fluid">
             <div className="row">
-                <div className="col-lg-6">
-                    <p>Donated To</p>
-                    <p>{campaigns} Campaigns</p>
+                <div className="col-lg-6 donation-history-summary-single">
+                    <div className="media donation-history-summary-media">
+                        <img className="img-responsive donation-history-icon" src="https://i.imgur.com/T6OG9vf.png"></img>
+                        <div className="media-body">
+                            <h3 className="donation-counter">{campaigns}</h3>
+                            <p className="text-center">NUMBER OF DONATIONS</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-lg-6">
-                    <p>Total Donation Amount</p>
-                    <p>${amount}</p>
+                <div className="col-lg-6 donation-history-summary-single">
+                <div className="media donation-history-summary-media">
+                        <img className="img-responsive donation-history-icon" src="https://i.imgur.com/0Smdpow.png"></img>
+                        <div className="media-body">
+                            <h3 className="donation-counter">{amount}</h3>
+                            <p className="text-center">TOTAL AMOUNT DONATED</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
